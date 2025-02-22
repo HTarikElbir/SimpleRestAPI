3c51444bb23ae5be02bf983ac739026a107b2888
HTarık <244035008@ogr.cbu.edu.tr>
2025-02-22 23:05:03 +0300

    Get methods added

--- SimpleRestAPI/Controllers/SchoolsController.cs
+++ SimpleRestAPI/Controllers/SchoolsController.cs
@@ -3,20 +3,53 @@ using SimpleRestAPI.Models;
 using SimpleRestAPI.Data;
 using Microsoft.EntityFrameworkCore;
 using System.Globalization;
+using Mapster;
 
 namespace SimpleRestAPI.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class SchoolsController(SchoolsDbContext context) : ControllerBase
+    public class SchoolsController : ControllerBase
     {
-        private readonly SchoolsDbContext _context = context;
+        private readonly SchoolsDbContext _context;
 
+        public SchoolsController(SchoolsDbContext context)
+        {
+            _context = context;
+        }
 
         [HttpGet]
         public async Task<ActionResult<List<School>>> GetSchools()
         {
-            return  Ok(await _context.Schools.ToListAsync());
+            var schools = await _context.Schools.ToListAsync();
+
+            if (schools == null)
+            {
+                return NotFound();
+            }
+            var response = schools.Adapt<List<SchoolDTO>>();
+            return Ok(response);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<School>> GetSchool(int id)
+        {
+            var school = await GetSchoolById(id);
+
+            if (school == null)
+            {
+                return NotFound();
+            }
+
+            var response = school.Adapt<SchoolDTO>();
+
+            return Ok(response);
+        }
+
+        private async Task<School?> GetSchoolById(int id)
+        {
+            var school = await _context.Schools.FindAsync(id);
+            return school;
         }
     }
 }
