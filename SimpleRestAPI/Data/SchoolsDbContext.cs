637b0ae8ec0347fd99e2d96666d5fb450186337a
HTarık <244035008@ogr.cbu.edu.tr>
2025-02-22 03:36:27 +0300

    DbContext definition and default connection configuration have been set.

--- SimpleRestAPI/Data/SchoolsDbContext.cs
+++ SimpleRestAPI/Data/SchoolsDbContext.cs
@@ -1,6 +1,11 @@
-﻿namespace SimpleRestAPI.Data
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using SimpleRestAPI.Models;
+
+namespace SimpleRestAPI.Data
 {
-    public class SchoolsDbContext
+    public class SchoolsDbContext(DbContextOptions<SchoolsDbContext> options) :DbContext(options)
     {
+        public DbSet<School> Hotels => Set<School>();
     }
 }
--- SimpleRestAPI/Program.cs
+++ SimpleRestAPI/Program.cs
@@ -1,16 +1,22 @@
+using Microsoft.EntityFrameworkCore;
+using Scalar.AspNetCore;
+using SimpleRestAPI.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
+builder.Services.AddDbContext<SchoolsDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.MapScalarApiReference();
     app.MapOpenApi();
 }
 
